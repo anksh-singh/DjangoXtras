6be27a136ddcf1a429859cccc7620aa64d509e23
ankitsingh1999 <ankitsi031999@gmail.com>
2022-04-20 12:00:00 +0000

    wsgi environ update

--- cookies/wsgi.py
+++ cookies/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cookies.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cookies.settings')
 
-# application = get_wsgi_application()
+application = get_wsgi_application()
